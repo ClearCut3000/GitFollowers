be4ea014604a1ac786ae911a3a4c7a44ae9a2bd4
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-07-14 22:47:07 +0300

    Added appIconSet. Added tabBarController with navController's.

---
+++ GitFollowers/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListViewController.swift
+//  GitFollowers
+//
+//  Created by Николай Никитин on 14.07.2022.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      view.backgroundColor = .systemCyan
+    }
+
+}
---
+++ GitFollowers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GitFollowers
+//
+//  Created by Николай Никитин on 14.07.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      view.backgroundColor = .systemMint
+    }
+    
+
+}
