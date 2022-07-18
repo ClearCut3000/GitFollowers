b767c2cd1bebef19f104f84409a29c1d07fbfa78
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-07-19 00:46:37 +0300

    Added collectionView.

--- GitFollowers/ViewControllers/FollowerListViewController.swift
+++ GitFollowers/ViewControllers/FollowerListViewController.swift
@@ -11,12 +11,34 @@ class FollowerListViewController: UIViewController {
 
   //MARK: - Properties
   var username: String!
+  var collectionView: UICollectionView!
 
   //MARK: - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureViewController()
+    configureCollectionView()
+    getFollowers()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: true)
+  }
+
+  //MARK: - Methods
+  func configureViewController() {
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
+  }
+
+  func configureCollectionView() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+    view.addSubview(collectionView)
+    collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+  }
+
+  func getFollowers() {
     NetworkManager.shared.getFollowers(for: username, page: 1) { result in
       switch result {
       case .success(let followers):
@@ -25,10 +47,5 @@ class FollowerListViewController: UIViewController {
         self.presentGFAlertOnMailThread(title: "Bad stuff Happened", message: error.rawValue, buttonTitle: "OK")
       }
     }
-  }
-
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    navigationController?.setNavigationBarHidden(false, animated: true)
   }
 }
