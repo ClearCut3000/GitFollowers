b497d68e75651975bac96ba63c887647773e22c4
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-07-21 00:53:02 +0300

    Added loading view

--- GitFollowers/Extensions/UIViewController+Extension.swift
+++ GitFollowers/Extensions/UIViewController+Extension.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
 
   func presentGFAlertOnMailThread(title: String, message: String, buttonTitle: String) {
@@ -18,4 +20,28 @@ extension UIViewController {
     }
   }
 
+  func showLoadingView() {
+    containerView = UIView(frame: view.bounds)
+    view.addSubview(containerView)
+    containerView.backgroundColor = .systemBackground
+    containerView.alpha = 0
+    UIView.animate(withDuration: 0.25) {
+      containerView.alpha = 0.8
+      let activityIndicator = UIActivityIndicatorView(style: .large)
+      containerView.addSubview(activityIndicator)
+      activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+      NSLayoutConstraint.activate([
+        activityIndicator.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+      ])
+      activityIndicator.startAnimating()
+    }
+  }
+
+  func dismissLoadinView() {
+    DispatchQueue.main.async {
+      containerView.removeFromSuperview()
+      containerView = nil
+    }
+  }
 }
