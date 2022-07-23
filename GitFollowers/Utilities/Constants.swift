469ee3390f63c8f409663b8361972712661f0b0a
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-07-23 23:38:32 +0300

    Added itemInfo view

--- GitFollowers/Utilities/Constants.swift
+++ GitFollowers/Utilities/Constants.swift
@@ -9,4 +9,8 @@ import Foundation
 
 enum SFSymbols {
   static let location = "mappin.and.ellipse"
+  static let repos = "folder"
+  static let gists = "text.alignment"
+  static let followers = "heart"
+  static let following = "person.2"
 }
--- GitFollowers/ViewControllers/UserInfoViewController.swift
+++ GitFollowers/ViewControllers/UserInfoViewController.swift
@@ -14,13 +14,44 @@ class UserInfoViewController: UIViewController {
 
   //MARK: - Subviews
   let headerView = UIView()
+  let itemViewOne = UIView()
+  let itemViewTwo = UIView()
+  var itemViews: [UIView] = []
 
   //MARK: - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
-    navigationItem.rightBarButtonItem = doneButton
+    confifureViewController()
     layoutUI()
+    getUserInfo()
+  }
+
+  //MARK: - Methods
+  func layoutUI() {
+    let padding:CGFloat = 20
+    itemViews = [headerView, itemViewOne, itemViewTwo]
+    for itemView in itemViews {
+      view.addSubview(itemView)
+      itemView.translatesAutoresizingMaskIntoConstraints = false
+      NSLayoutConstraint.activate([
+        itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+        itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+      ])
+    }
+    let itemHeight: CGFloat = 140
+    NSLayoutConstraint.activate([
+      headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      headerView.heightAnchor.constraint(equalToConstant: 180),
+
+      itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+      itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+
+      itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+      itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight)
+    ])
+  }
+
+  func getUserInfo() {
     NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
       guard let self = self else { return }
       switch result {
@@ -34,15 +65,10 @@ class UserInfoViewController: UIViewController {
     }
   }
 
-  //MARK: - Methods
-  func layoutUI() {
-    view.addSubview(headerView)
-    NSLayoutConstraint.activate([
-      headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-      headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      headerView.heightAnchor.constraint(equalToConstant: 180)
-    ])
+  func confifureViewController() {
+    view.backgroundColor = .systemBackground
+    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+    navigationItem.rightBarButtonItem = doneButton
   }
 
   func add(childVC: UIViewController, to containerView: UIView) {
