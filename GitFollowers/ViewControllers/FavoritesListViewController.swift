6314b53c4582bc131be17475a4d26f12e00bf068
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-07-27 00:08:31 +0300

    Added swipe-to-delete from tableView

--- GitFollowers/ViewControllers/FavoritesListViewController.swift
+++ GitFollowers/ViewControllers/FavoritesListViewController.swift
@@ -75,4 +75,24 @@ extension FavoritesListViewController: UITableViewDataSource, UITableViewDelegat
     cell.set(favorite: favorite)
     return cell
   }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let favorite = favorites[indexPath.row]
+    let destinationVC = FollowerListViewController()
+    destinationVC.username = favorite.login
+    destinationVC.title = favorite.login
+    navigationController?.pushViewController(destinationVC, animated: true)
+  }
+
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    guard editingStyle == .delete else { return }
+    let favorite = favorites[indexPath.row]
+    favorites.remove(at: indexPath.row)
+    tableView.deleteRows(at: [indexPath], with: .left)
+    PersistanceManager.update(with: favorite, actionType: .remove) { [weak self] error in
+      guard let self = self else { return }
+      guard let error = error else { return }
+      self.presentGFAlertOnMailThread(title: "Unable to delete favorite!", message: error.rawValue, buttonTitle: "OK")
+    }
+  }
 }
